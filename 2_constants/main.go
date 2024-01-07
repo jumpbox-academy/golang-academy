46ca50ec82c7d8f678b9bcb17986af26dcfea20e
sikiryl <saritrat.206@gmail.com>
2024-01-07 10:46:40 +0700

    feat: add constants

---
+++ 2_constants/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const INT_VALUE = 6
+	const STRING_VALUE = "Jumpbox"
+	const BOOL_VALUE = true
+
+	const (
+		sunday = iota
+		monday
+		tuesday
+		wednesday
+		thursday
+		friday
+		saturday
+	)
+	fmt.Println("====  constants ====")
+	fmt.Println("INT_VALUE: ", INT_VALUE)
+	fmt.Println("STRING_VALUE: ", STRING_VALUE)
+	fmt.Println("BOOL_VALUE: ", BOOL_VALUE, "\n")
+
+	fmt.Println("==== Enum with constant ====")
+	fmt.Println("enum - monday: ", monday)
+	fmt.Println("enum - tuesday: ", tuesday)
+	fmt.Println("enum - wednesday: ", wednesday)
+
+}

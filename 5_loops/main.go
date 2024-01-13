c645465aee099432ae407788cd0edd6545de7be7
sikiryl <saritrat.206@gmail.com>
2024-01-13 17:30:58 +0700

    feat: add loops

---
+++ 5_loops/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Instruction of this example:
+	// if you want to test each scenario.
+	// please comment and uncomment that you want.
+
+	// Scenario 1:
+	// loop with 3 components (for)
+	for i := 0; i < 10; i++ {
+		fmt.Println("i: ", i)
+	}
+
+	// Scenario 2:
+	// loop with a condition (while)
+	// j := 5
+	// for j < 10 {
+	// 	fmt.Println("j: ", j)
+	// 	j++
+	// }
+
+	// Scenario 3:
+	// infinite loop
+	// for {
+	// 	fmt.Println("forever loop")
+	// }
+}

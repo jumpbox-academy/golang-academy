66900765468cd54b6bb5260290d23d70dc96082b
sikiryl <saritrat.206@gmail.com>
2024-01-22 07:31:55 +0700

    feat: connect to sql db

---
+++ 21_connect_std_db/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
